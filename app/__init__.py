4098662968af194d355e2973a44b7781b01fc512
Shane Victor <batoonshanevictor@yahoo.com>
2025-09-20 16:02:27 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 from app.config import Config
 from app.models import db
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='../templates')
     app.config.from_object(Config)
 
     db.init_app(app)
--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@localhost/airbnb_dashboard'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:admin@localhost/airbnb_dashboard'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
